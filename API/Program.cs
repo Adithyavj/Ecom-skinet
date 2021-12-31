4373eda2261bb94738d607501a08da39aa5fdc57
Adithya Vijay K <adithyavj@gmail.com>
2022-01-01 01:26:20 +0530

    Adding seed data

--- API/Program.cs
+++ API/Program.cs
@@ -10,18 +10,19 @@ namespace API
             var host = CreateHostBuilder(args).Build();
             using (var scope = host.Services.CreateScope())
             {
-                 var services = scope.ServiceProvider;
-                 var loggerFactory = services.GetRequiredService<ILoggerFactory>();
-                 try
-                 {
-                      var context = services.GetRequiredService<StoreContext>();
-                      await context.Database.MigrateAsync();
-                 }
-                 catch (Exception ex)
-                 {                     
-                     var logger = loggerFactory.CreateLogger<Program>();
-                     logger.LogError(ex, "An error occured during migration");
-                 }
+                var services = scope.ServiceProvider;
+                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+                try
+                {
+                    var context = services.GetRequiredService<StoreContext>();
+                    await context.Database.MigrateAsync();
+                    await StoreContextSeed.SeedAsync(context, loggerFactory);
+                }
+                catch (Exception ex)
+                {
+                    var logger = loggerFactory.CreateLogger<Program>();
+                    logger.LogError(ex, "An error occured during migration");
+                }
             }
             host.Run();
         }
